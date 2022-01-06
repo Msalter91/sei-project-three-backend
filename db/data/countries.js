129cf467a4859f554588ee65198e3e5eea087cae
Mike Salter <mikey.l.salter@gmail.com>
2022-01-06 15:20:09 +0000

    new seed data for countries

--- db/data/countries.js
+++ db/data/countries.js
@@ -1,6 +1,100 @@
 export default [
   {
+    name: 'Afghanistan',
+    countrycode: 'AFG',
+    image: 'https://XXX',
+    summary: 'Afghanistan officially the Islamic Emirate of Afghanistan, is a landlocked country located at the crossroads of Central and South Asia.  It is bordered by Pakistan to the east and south, Iran to the west, Turkmenistan and Uzbekistan to the north, and Tajikistan and China to the northeast.  Occupying 652,864 square kilometers (252,072 sq mi) of land, the country is predominately mountainous with plains in the north and the southwest that are separated by the Hindu Kush mountain range.  As of 2021, its population is 40.2 million, composed mostly of ethnic Pashtuns, Tajiks, Hazaras, and Uzbeks. Kabul is the country\'s largest city and serves as its capital.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Albania',
+    countrycode: 'ALB',
+    image: 'https://XXX',
+    summary: 'Albania officially the Republic of Albania, is a country in Southeastern Europe.  It is located on the Adriatic and Ionian Sea within the Mediterranean Sea and shares land borders with Montenegro to the northwest, Kosovo to the northeast, North Macedonia to the east and Greece to the south.  Tirana is its capital and largest city, followed by Durrës, Vlorë and Shkodër.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Algeria',
+    countrycode: 'DZA',
+    image: 'https://XXX',
+    summary: 'Algeria, officially the People\'s Democratic Republic of Algeria, is a country in the Maghreb region of North Africa.  It is the largest country by total area in Africa and in the Arab world. It is bordered to the northeast by Tunisia; to the east by Libya; to the southeast by Niger; to the southwest by Mali, Mauritania, and Western Sahara; to the west by Morocco; and to the north by the Mediterranean Sea.  The country has a semi-arid geography, with most of the population living in the fertile north and the Sahara dominating the geography of the south.  Algeria covers an area of 2,381,741 square kilometres (919,595 sq mi), making it the world\'s tenth largest nation by area. With a population of 44 million, Algeria is the ninth-most populous country in Africa, and the 32nd-most populous country in the world.  The capital and largest city is Algiers, located in the far north on the Mediterranean coast.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'American Samoa',
+    countrycode: 'ASM',
+    image: 'https://XXX',
+    summary: 'American Samoa (Samoan: Amerika Sāmoa; also Amelika Sāmoa or Sāmoa Amelika) is an unincorporated territory of the United States located in the South Pacific Ocean, southeast of the independent state of Samoa.  Its location is centered on 14.3°S 170.7°W. It is east of the International Date Line, while Samoa is west of the Line.  The total land area is 199 square kilometers (76.8 sq mi), slightly more than Washington, D.C. American Samoa is the southernmost territory of the United States and one of two U.S. territories south of the Equator, along with the uninhabited Jarvis Island.  Tuna products are the main exports, and the main trading partner is the rest of the United States.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Andorra',
+    countrycode: 'AND', 
+    image: 'https://XXX',
+    summary: 'Andorra, officially the Principality of Andorra, is a sovereign landlocked microstate on the Iberian Peninsula, in the eastern Pyrenees, bordered by France to the north and Spain to the south.  Believed to have been created by Charlemagne, Andorra was ruled by the count of Urgell until 988, when it was transferred to the Roman Catholic Diocese of Urgell. The present principality was formed by a charter in 1278.  It is headed by two co-princes: the Bishop of Urgell in Catalonia, Spain and the President of France.  Its capital and largest city is Andorra la Vella.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Angola',
+    countrycode: 'AGO',
+    image: 'https://XXX',
+    summary: 'Angola , officially the Republic of Angola (Portuguese: República de Angola), is a country on the west coast of Southern Africa.  It is the second-largest Lusophone (Portuguese-speaking) country in both total area and population (behind Brazil), and is the seventh-largest country in Africa.  It is bordered by Namibia to the south, the DR Congo to the north, Zambia to the east, and the Atlantic Ocean to the west.  Angola has an exclave province, the province of Cabinda, that borders the Republic of the Congo and the Democratic Republic of the Congo. The capital and most populated city is Luanda.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Anguilla',
+    countrycode: 'AIA',
+    image: 'https://XXX',
+    summary: 'Anguilla is a British overseas territory in the Caribbean.  It is one of the most northerly of the Leeward Islands in the Lesser Antilles, lying east of Puerto Rico and the Virgin Islands and directly north of Saint Martin.   The territory consists of the main island of Anguilla, approximately 16 miles (26 kilometres) long by 3 miles (5 km) wide at its widest point, together with a number of much smaller islands and cays with no permanent population. The territory\'s capital is The Valley.  The total land area of the territory is 35 square miles (91 km2), with a population of approximately 14,731 (2018). ',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Antarctica',
+    countrycode: 'ATA',
+    image: 'https://XXX',
+    summary: 'Antarctica is Earth\'s southernmost continent.  It contains the geographic South Pole and is situated in the Antarctic region of the Southern Hemisphere, almost entirely south of the Antarctic Circle, and is surrounded by the Southern Ocean. At 14,200,000 square kilometres (5,500,000 square miles), it is the fifth-largest continent and nearly twice the size of Australia. It is by far the least populated continent, with around 5,000 people in the summer and around 1,000 in the winter. About 98% of Antarctica is covered by ice that averages 1.9 km (1.2 mi; 6,200 ft) in thickness.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Antigua and Barbuda',
+    countrycode: 'ATG',
+    image: 'https://XXX',
+    summary: 'Antigua and Barbuda; is a sovereign island country in the West Indies in the Americas, lying between the Caribbean Sea and the Atlantic Ocean.  It consists of two major islands, Antigua and Barbuda separated by 63 km (39 mi), and smaller islands (including Great Bird, Green, Guiana, Long, Maiden, Prickly Pear, York Islands, Redonda).  The permanent population number is about 97,120 (2019 est.), with 97% being resident on Antigua.  The capital and largest port and city is St. John\'s on Antigua, with Codrington being the largest town on Barbuda.  Lying near each other, Antigua and Barbuda are in the middle of the Leeward Islands, part of the Lesser Antilles, roughly at 17°N of the equator.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
+    name: 'Argentina',
+    countrycode: 'ARG',
+    image: 'https://XXX',
+    summary: 'Argentina, officially the Argentine Republic (Spanish: República Argentina), is a country in the southern half of South America.  Argentina covers an area of 2,780,400 km2 (1,073,500 sq mi), making it the largest Spanish-speaking nation in the world by area.  It is the second-largest country in South America after Brazil, the fourth-largest country in the Americas, and the eighth-largest country in the world.  It shares the bulk of the Southern Cone with Chile to the west, and is also bordered by Bolivia and Paraguay to the north, Brazil to the northeast, Uruguay and the South Atlantic Ocean to the east, and the Drake Passage to the south.  Argentina is a federal state subdivided into twenty-three provinces, and one autonomous city, which is the federal capital and largest city of the nation, Buenos Aires.  The provinces and the capital have their own constitutions, but exist under a federal system.  Argentina claims sovereignty over a part of Antarctica, the Falkland Islands and South Georgia and the South Sandwich Islands.',  
+    language: 'tbc',
+    currency: 'tbc',
+  },
+
+  {
     name: 'Japan',
-    image: 'https://ychef.files.bbci.co.uk/1600x900/p06y270b.webp',
+    countrycode: 'JPN',
+    image: 'https://XXX',
+    summary: 'The capital of Japan is Tokyo, Japan (Japanese: 日本, Nippon or Nihon, and formally 日本国) is an island country in East Asia.  It is situated in the northwest Pacific Ocean, and is bordered on the west by the Sea of Japan, while extending from the Sea of Okhotsk in the north toward the East China Sea and Taiwan in the south.  Japan is a part of the Ring of Fire, and spans an archipelago of 6852 islands covering 377,975 square kilometers (145,937 sq mi); the five main islands are Hokkaido, Honshu (the "mainland"), Shikoku, Kyushu, and Okinawa.  Tokyo is the nation\'s capital and largest city; other major cities include Yokohama, Osaka, Nagoya, Sapporo, Fukuoka, Kobe, and Kyoto.',  
+    language: 'tbc',
+    currency: 'tbc',
   }
 ]
--- models/country.js
+++ models/country.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose'
 const countrySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   image: { type: String },
+  countrycode: { type: String, required: true },
+  summary: { type: String, required: true },
+  language: { type: String, required: true },
+  currency: { type: String, required: true },
 })
 
 export default mongoose.model('Country', countrySchema)
