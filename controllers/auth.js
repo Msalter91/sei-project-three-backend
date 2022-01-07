fc0215fa71f3c8fc72ffb228767dfe224cce9bb5
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-07 12:50:34 +0000

    do not allow admin usernames

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,15 @@ import User from '../models/user.js'
 
 import jwt from 'jsonwebtoken'
 import { jwtSecret } from '../config/environment.js'
+import { Unauthorized } from '../lib/errors.js'
 
 async function register(req, res, next) {
   try { 
+    if ((req.body.displayName).toLowerCase().includes('admin')){
+      throw new Unauthorized
+    }
+    
+    console.log('registering user:', req.body.displayName)
     const createdUser = await User.create(req.body)
     return res.status(201).json({
       message: `Welcome ${createdUser.displayName}`,
@@ -16,12 +22,20 @@ async function register(req, res, next) {
 
 async function login (req, res, next) {
   try {
+    if (req.body.password) throw new Unauthorized()
+
     const user = await User.findOne( {
       email: req.body.email })
     if (!user || !user.validatePassword(req.body.password)) {
-      throw new Error
+      throw new Unauthorized()
     }
-    const token = jwt.sign( { sub: user._id }, jwtSecret, { expiresIn: '24 hours' } )
+
+    const token = jwt.sign(
+      { sub: user._id },
+      jwtSecret,
+      { expiresIn: '24 hours' } 
+    )
+
     return res.status(202).json( { message: 'welcome back', token })
   } catch (err) {
     next(err)
