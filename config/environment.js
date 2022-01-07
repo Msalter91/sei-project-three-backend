1f66e017abb752e35bc6aabdc70725205a0b49dd
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-07 11:46:25 +0000

    rename jwtSecret

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 export const dbName = 'placebook-db'
 export const dbURI = `mongodb://127.0.0.1/${dbName}`
 export const port = 4000
-export const secret = 'elephantbongotriumph'
+export const jwtSecret = 'elephantbongotriumph'
 export const adminPW = 'testytestytest'
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 import User from '../models/user.js'
 
 import jwt from 'jsonwebtoken'
-import { secret } from '../config/environment.js'
+import { jwtSecret } from '../config/environment.js'
 
 async function register(req, res, next) {
   try { 
@@ -21,7 +21,7 @@ async function login (req, res, next) {
     if (!user || !user.validatePassword(req.body.password)) {
       throw new Error
     }
-    const token = jwt.sign( { sub: user._id }, secret, { expiresIn: '24 hours' } )
+    const token = jwt.sign( { sub: user._id }, jwtSecret, { expiresIn: '24 hours' } )
     return res.status(202).json( { message: 'welcome back', token })
   } catch (err) {
     next(err)
