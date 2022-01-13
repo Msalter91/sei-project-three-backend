6f9945999a396d562b37c15e2bea946ea1a79a9c
Mike Salter <mikey.l.salter@gmail.com>
2022-01-13 15:11:41 +0000

    deploy fix

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@ dotenv.config()
 
 export const dbName = 'placebook-db'
 export const dbURI = process.env.DB_URI || `mongodb://127.0.0.1/${dbName}`
+console.log(dbURI)
 export const port = process.env.PORT || 4000
 export const jwtSecret = process.env.JWT_SECRET || 'elephantbongotriumph'
 export const adminPW = process.env.ADMIN_PASS || 'testytestytest'
