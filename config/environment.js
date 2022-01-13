04c1f7a00acbfceb8d8bfb96c6ca93db3f1b16ca
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-13 14:32:45 +0000

    port

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,6 @@ dotenv.config()
 
 export const dbName = 'placebook-db'
 export const dbURI = process.env.DB_URI || `mongodb://127.0.0.1/${dbName}`
-export const port = process.env.DB_PORT || 4000
+export const port = process.env.PORT || 4000
 export const jwtSecret = process.env.JWT_SECRET || 'elephantbongotriumph'
 export const adminPW = process.env.ADMIN_PASS
